6a777741659782a18d4484fee461465d5c6f0a21
Easonnaly <18888046840@163.com>
2019-11-08 13:17:46 +0800

    第二次提交

--- src/main/java/com/eason/api_member/api/service/impl/MemberServiceImpl.java
+++ src/main/java/com/eason/api_member/api/service/impl/MemberServiceImpl.java
@@ -5,7 +5,6 @@ import com.eason.api_member.api.service.MemberService;
 import com.eason.api_member.base.BaseService;
 import com.eason.api_member.entity.UserEntity;
 import com.eason.api_member.utils.MD5Util;
-import com.sun.deploy.net.proxy.pac.PACFunctions;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
